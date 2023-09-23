e93ed23ce314452f1bb1402484f1b62eae1deb16
solidDoWant <fred.heinecke@yahoo.com>
2023-09-22 22:10:13 -0500

    Updated proejct to include a valid module type example with a custom OBJ model

---
+++ ModuleTypeTest.cs
@@ -0,0 +1,36 @@
+﻿using Planetbase;
+using PlanetbaseFramework;
+using UnityEngine;
+
+namespace TestMod
+{
+    // The class name is important! The Planetbase code does some funky type and reflection logic based upon the class name.
+    // Non-abstract classes should always begin with "ModuleType". The in-game name of the module type is determined by
+    // a "strings" (localization) lookup for the class suffix. For example, the in-game name of this module will be
+    // "Test module", which is determined by getting the class name suffix ("Test"), converting it to lower case ("test"),
+    // and looking up the corresponding string which is set in assets/strings/testmod_en.xml.
+    class ModuleTypeTest : BaseModuleType
+    {
+        public ModuleTypeTest(Texture2D icon, GameObject[] models) : base(icon, models)
+        {
+            mPowerGeneration = -1000;                               // How much power each module instance produces (positive value) or consumes (negative value)
+            mExterior = false;                                      // Colonist can walk in interior structures
+            mHeight = 1f;
+            mRequiredStructure.set<ModuleTypeOxygenGenerator>();    // This controls what structure is required to be built before this one may be placed
+            mFlags = FlagDome + FlagLightAtNight + FlagWalkable;    // Flags control misc. properties of the module
+            mLayoutType = LayoutType.Circular;                      // This controls where colonists will path
+        }
+
+        public override ResourceAmounts calculateCost(int sizeIndex)
+        {
+            var resources = new ResourceAmounts();
+
+            var adjustedSizeIndex = sizeIndex + 1;  // By default size indices are 0-based
+
+            resources.add(TypeList<ResourceType, ResourceTypeList>.find<Metal>(), adjustedSizeIndex * 2);
+            resources.add(TypeList<ResourceType, ResourceTypeList>.find<Bioplastic>(), adjustedSizeIndex * 3);
+
+            return resources;
+        }
+    }
+}
--- TestMod.cs
+++ TestMod.cs
@@ -1,24 +1,36 @@
 ﻿using Planetbase;
 using PlanetbaseFramework;
-using System;
-using UnityEngine;
 
 namespace TestMod
 {
     public class TestMod : ModBase
     {
-        public TestMod() : base("Test mod")
-        {
-        }
+        public override string ModName => "Test mod";
 
         public override void Init()
         {
-            TypeList<ModuleType, ModuleTypeList> moduleList = TypeList<ModuleType, ModuleTypeList>.getInstance();
-            moduleList.add(new TestModuleType());
+            RegisterNewModuleTypes();
         }
 
-        public override void Update()
+        public void RegisterNewModuleTypes()
         {
+            // This is a global Planetbase object that contains all the ModuleTypes that players can choose from
+            var moduleList = TypeList<ModuleType, ModuleTypeList>.getInstance();
+            moduleList.add(
+                // Note that these files are marked as "embedded resources" in the Visual Studio project.
+                // See the TestMod.csproj file for details.
+                new ModuleTypeTest(
+                    // This is the icon that will be used on the module selection screen.
+                    ModTextures.FindTextureWithName("icon.png"),
+                    // This is an array of GameObjects that will be used as models for each module instance.
+                    // Each entry in the array corresponds with a "size" of the module, which players can select
+                    // by scrolling up and down when placing.
+                    new[]
+                    {
+                        ModObjects.FindObjectByFilename("test.obj")
+                    }
+                )
+            );
         }
     }
 }
