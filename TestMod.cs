ded1212c4d722ce5d6051caee1270288e1ec19d2
solidDoWant <fred.heinecke@yahoo.com>
2023-10-06 00:33:40 -0500

    Updated to show `ModuleModelBuilder` usage

--- TestMod.cs
+++ TestMod.cs
@@ -1,5 +1,8 @@
-﻿using Planetbase;
+﻿using System.Collections.Generic;
+using Planetbase;
 using PlanetbaseFramework;
+using PlanetbaseFramework.GameMechanics.Buildings;
+using UnityEngine;
 
 namespace TestMod
 {
@@ -14,23 +17,33 @@ namespace TestMod
 
         public void RegisterNewModuleTypes()
         {
+            // Note that ModTexture and ModObject files are marked as "embedded resources" in the Visual Studio project.
+            // See the TestMod.csproj file for details.
+
+            // Create a model builder that can be used to produce models with the specified configuration
+            var modelBuilder = new ModuleModelBuilder().AddPreStructuredObjects(ModObjects.FindObjectByFilename("test.obj"));
+
+            // Add collision to appropriate parts of the model
+            foreach (var floorObject in modelBuilder.FloorObjects)
+                ModuleModelBuilder.AddCollisionGeometry(floorObject);
+
+            // Generate several models with different sizes
+            var models = new List<GameObject>();
+            for (float i = 1; i <= 2; i += 0.5f)
+            {
+                var model = modelBuilder.GenerateObject($"size_scaled_{i}");
+                model.transform.localScale = i * Vector3.one;
+                models.Add(model);
+            }
+
+            // Create the new object type, providing the UI icon and models for each size
+            var testModuleType = new ModuleTypeTest(ModTextures.FindTextureWithName("icon.png"), models.ToArray());
+
             // This is a global Planetbase object that contains all the ModuleTypes that players can choose from
             var moduleList = TypeList<ModuleType, ModuleTypeList>.getInstance();
-            moduleList.add(
-                // Note that these files are marked as "embedded resources" in the Visual Studio project.
-                // See the TestMod.csproj file for details.
-                new ModuleTypeTest(
-                    // This is the icon that will be used on the module selection screen.
-                    ModTextures.FindTextureWithName("icon.png"),
-                    // This is an array of GameObjects that will be used as models for each module instance.
-                    // Each entry in the array corresponds with a "size" of the module, which players can select
-                    // by scrolling up and down when placing.
-                    new[]
-                    {
-                        ModObjects.FindObjectByFilename("test.obj")
-                    }
-                )
-            );
+
+            // Register the new module type
+            moduleList.add(testModuleType);
         }
     }
 }
